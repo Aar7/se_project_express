1ccbe092a67ca83f87cab0ac1699e35b0fd6bf48
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-12 14:46:02 -0600

    (feat) set CORS headers

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ mongoose
   .catch(console.error);
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://aarwtwr.fairuse.org");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use(requestLogger);
 app.get("/crash-test", () => {

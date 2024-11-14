f6c05fd715e1d4a2d941020e5199750d324394a1
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-14 11:32:53 -0600

    (fix) xomply with CEH in routes/index

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,14 @@ const router = require("express").Router();
 const signinRouter = require("./signins");
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
-
-const { NOT_FOUND_CODE } = require("../utils/errors");
 const auth = require("../middlewares/auth");
+const NotFoundError = require("../middlewares/NotFoundError");
 
 router.use("/", signinRouter);
 router.use("/users", auth, userRouter);
 router.use("/items", clothingItemsRouter);
 router.use((req, res) => {
-  res.status(NOT_FOUND_CODE).send({ message: "Route does not exist" });
+  throw new NotFoundError("Route does not exist");
 });
 
 module.exports = router;

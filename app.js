df1bbaefa5321a34354421e2aad3555c40753647
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-14 11:56:21 -0600

    (feat) add helmet node module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { errors } = require("celebrate");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
@@ -30,6 +31,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use(helmet());
+app.disable("x-powered-by");
 app.use(limiter);
 app.use(express.json());
 app.use(requestLogger);

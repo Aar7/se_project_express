a07eb810ee204ef00f90c9b1acf9494f3fcc0116
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-11-16 21:14:56 -0600

    (fix) allow unknown fields in new card validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -17,18 +17,20 @@ const validateEmail = (value, helpers) => {
   return helpers.error("any.invalid");
 };
 module.exports.validateItemInfo = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
-      "string.min": 'The minimum length of the "name" field is 2',
-      "string.max": 'The maximum length of the "name" field is 30',
-      "string.empty": 'The "name" field must be filled in',
-    }),
-    weather: Joi.string().required().valid("hot", "warm", "cold"),
-    imageUrl: Joi.string().required().custom(validateURL).messages({
-      "string.uri": 'the "imageUrl" field must be a valid url',
-      "string.empty": 'The "imageUrl" field must be filled in',
-    }),
-  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
+      weather: Joi.string().required().valid("hot", "warm", "cold"),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        "string.uri": 'the "imageUrl" field must be a valid url',
+        "string.empty": 'The "imageUrl" field must be filled in',
+      }),
+    })
+    .unknown(true),
 });
 
 module.exports.validateNewUserInfo = celebrate({
